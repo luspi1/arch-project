8fbcb65f17a50b3fe9ff1b02236b91459b707a20
Владелец <igbabin566@gmail.com>
2022-11-22 12:11:38 +0300

    Блок контакты: маски для инпутов, активные табы, кастомный чекбокс, блок "Лицензии и СРО"

---
+++ src/js/components/contactTabs.js
@@ -0,0 +1,23 @@
+const contactBtnsWrapper = document.querySelector('.contact-us__choice-controllers')
+
+const showTargetTab = (newDataTab, oldDataTab) => {
+  const newTargetTab = document.querySelector(`.contact-us__choice-item[data-tab="${newDataTab}"]`)
+  const oldTargetTab = document.querySelector(`.contact-us__choice-item[data-tab="${oldDataTab}"]`)
+
+  if (newTargetTab && oldTargetTab) {
+    newTargetTab.classList.add('contact-us__choice-item_active')
+    oldTargetTab.classList.remove('contact-us__choice-item_active')
+  }
+}
+
+
+if (contactBtnsWrapper) {
+  contactBtnsWrapper.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('contact-us__choice-btn_active') && !e.target.classList.contains('contact-us__choice-controllers')) {
+      const activeBtn = e.currentTarget.querySelector('.contact-us__choice-btn_active')
+      activeBtn.classList.remove(('contact-us__choice-btn_active'))
+      e.target.classList.add(('contact-us__choice-btn_active'))
+      showTargetTab(e.target.dataset.tab, activeBtn.dataset.tab)
+    }
+  })
+}
---
+++ src/js/components/inputMask.js
@@ -0,0 +1,12 @@
+import Inputmask from "inputmask";
+
+const phoneInputs = document.querySelectorAll('.phone-mask');
+
+if (phoneInputs) {
+  phoneInputs.forEach(input => {
+    Inputmask({
+      "mask": "+7 (999) 999-99-99",
+      showMaskOnHover: false,
+    }).mask(input);
+  })
+}
