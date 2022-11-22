15069492f8c292fe7987dadf1d9ed3e47b6b777b
VensKramskoy <venskramskoy@gmail.com>
2022-11-22 16:28:59 +0300

    сделан адаптив

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -5,6 +5,10 @@ const swiperServices = new Swiper(".swiper-services", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
 
 
@@ -14,6 +18,10 @@ const swiperFeatures = new Swiper(".swiper-features", {
     prevEl: ".swiper-button-prev",
   },
   slidesPerView: 1,
+  spaceBetween: 50,
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
 
 const swiperCommand = new Swiper(".swiper-command", {
@@ -21,8 +29,22 @@ const swiperCommand = new Swiper(".swiper-command", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 25,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
 });
 
 const swiperProjects = new Swiper(".swiper-projects", {
@@ -33,6 +55,9 @@ const swiperProjects = new Swiper(".swiper-projects", {
   spaceBetween: 350,
   slidesPerView: "auto",
   centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
 
 const swiperClients = new Swiper(".swiper-clients", {
@@ -40,9 +65,26 @@ const swiperClients = new Swiper(".swiper-clients", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  slidesPerView: 6,
-  spaceBetween: 52,
-  freeMode: true,
+  slidesPerView: 1,
+  spaceBetween: 70,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 52,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+    1400: {
+      slidesPerView: 6,
+    },
+  },
 });
 
 
