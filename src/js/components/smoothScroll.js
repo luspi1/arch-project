71e680087f15f5bf2642a00d7b1b3f39fa644520
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-03-16 12:45:23 +0300

    ограничение скролла только на главной

--- src/js/components/smoothScroll.js
+++ src/js/components/smoothScroll.js
@@ -1,44 +1,48 @@
 //плавная прокрутка
 
 const menuLinks = document.querySelectorAll('.main-menu__navigation a')
+const homePage = document.querySelector('.home-page')
 
-menuLinks.forEach(link => {
-	link.addEventListener('click', function (e) {
-		e.preventDefault()
+if (homePage) {
+  menuLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
 
-		const href = this.getAttribute('href').substring(1)
+      e.preventDefault()
+      const href = this.getAttribute('href').substring(1)
 
-		const scrollTarget = document.getElementById(href)
-		const elementPosition = scrollTarget.getBoundingClientRect().top
+      const scrollTarget = document.getElementById(href)
+      const elementPosition = scrollTarget.getBoundingClientRect().top
 
-		window.scrollBy({
-			top: elementPosition,
-			behavior: 'smooth',
-		})
-	})
-})
+      window.scrollBy({
+        top: elementPosition,
+        behavior: 'smooth',
+      })
+    })
+  })
+}
+
 
 //Прокрутка вверх
 
 const scrollBtn = document.querySelector('#scrollTop')
 
 window.addEventListener('scroll', e => {
-	e.preventDefault()
-	if (scrollBtn) {
-		if (window.scrollY > 600) {
-			scrollBtn.classList.remove('_hide')
-		} else {
-			scrollBtn.classList.add('_hide')
-		}
-	}
+  e.preventDefault()
+  if (scrollBtn) {
+    if (window.scrollY > 600) {
+      scrollBtn.classList.remove('_hide')
+    } else {
+      scrollBtn.classList.add('_hide')
+    }
+  }
 })
 
 if (scrollBtn) {
-	scrollBtn.addEventListener('click', e => {
-		e.preventDefault()
-		window.scrollBy({
-			top: -99999,
-			behavior: 'smooth',
-		})
-	})
+  scrollBtn.addEventListener('click', e => {
+    e.preventDefault()
+    window.scrollBy({
+      top: -99999,
+      behavior: 'smooth',
+    })
+  })
 }
